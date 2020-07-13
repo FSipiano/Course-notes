6c7f38757b8907f47d62d058c128e5d3b8584813
F Sipiano <67593261+FSipiano@users.noreply.github.com>
2020-07-13 16:17:43 +0100

    Update palindromeChecker.js

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -1,4 +1,8 @@
-function palindrome (''){
-palindrome.
-
+function palindrome (str){
+var noCharacters = /\W/g
+var lowCaseReplace= palindrome.toLowerCase().replace(noCharacters, '')
+var reverseStr = lowCaseReplace.split('').reverse().join;('')
+return reverseStr === lowCaseReplace
 }
+
+palindrome('racecar')
