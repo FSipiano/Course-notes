9f643674a76009721c8819dc3b89946272d810de
F Sipiano <67593261+FSipiano@users.noreply.github.com>
2020-07-14 16:18:32 +0100

    (parameters) name refactor

--- findEvenNumbers.js
+++ findEvenNumbers.js
@@ -2,18 +2,16 @@
 
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-let evenArray = []
+
 
 function findEvenNumbers(nums) {
-
+let evenArray = []
     for (i = 0; i < nums.length; i++) {
         if (nums[i] % 2 === 0) {
 
-            evenArray.push(arr[i])
+            evenArray.push(nums[i])
         }
     }
     return evenArray
 }
-
-
 console.log(findEvenNumbers(arr))
